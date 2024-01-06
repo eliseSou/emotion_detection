12546b502c1e84d6d33d074739b90092647306ae
Marie <marie.douet@insa-rouen.fr>
2024-01-06 16:34:02 +0100

    Add colors to text

--- interface-web/src/App.tsx
+++ interface-web/src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { useState } from 'react';
 import { useTranslation } from "react-i18next";
-import { Image, Text, Stack, HStack, VStack } from "@chakra-ui/react";
+import { Image, Text, Stack, HStack, VStack, color } from "@chakra-ui/react";
 import { EmotionsList, EmotionsEmoticon } from './constantsEmotions';
 
 function App() {
@@ -49,18 +49,28 @@ function App() {
           <Stack></Stack>
 
           <VStack justifyContent='center' width="95vw">
-            <Text marginTop='2vh' marginBottom='10vh' fontSize={35}>
-              {t('welcome.title')}
-            </Text>
+            <HStack marginTop='2vh' marginBottom='10vh' fontSize={35}>
+              <Text>
+                {t('welcome.titleBefore')}
+              </Text>
+              <Text bgGradient='linear(to-r, #FFB286, #FF0080)' bgClip='text' fontWeight='extrabold'>
+                Emoticall
+              </Text>
+              <Text>
+                {t('welcome.titleAfter')}
+              </Text>
+            </HStack>
             <VStack bg='#383b40' padding='5vh' borderRadius={30}>
               <Image width={500} height={500} marginBottom='2vh' src={srcEmotion} />
-              <Text fontSize={20}>{t('emotion.detected')}</Text>
-              <Text fontSize={30}>{t('emotion.list.' + `${detectedEmotion}`)}</Text>
+              <Text fontSize={25}>{t('emotion.detected')}</Text>
+              <Text fontSize={35} bgGradient='linear(to-r, #FFB286, #FF0080)' bgClip='text' fontWeight='extrabold'>
+                {t('emotion.list.' + `${detectedEmotion}`)}
+              </Text>
             </VStack>
 
             <Stack className='Footer' alignSelf='center'>
               <Text fontSize="sm">
-                &copy; {new Date().getFullYear()} Created by Marie DOUET, Brice GRINDEL, Lucas MARTIN & Élise SOUVANNAVONG
+                &copy; {new Date().getFullYear()} {t('welcome.footer')}
               </Text>
             </Stack>
           </VStack>
